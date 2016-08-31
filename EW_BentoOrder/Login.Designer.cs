d6f2de004ba496d848786eec25dd88e90857168f
coreychen71 <coreychen71@gmail.com>
2016-08-31 10:10:52 +0800

    2016/08/31 v4.2 1.廠長室新增一名掛名人員，將應到人數再扣1

--- EW_BentoOrder/Login.Designer.cs
+++ EW_BentoOrder/Login.Designer.cs
@@ -113,7 +113,7 @@
             this.MaximizeBox = false;
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Login v4.1";
+            this.Text = "Login v4.2";
             this.ResumeLayout(false);
             this.PerformLayout();
 
